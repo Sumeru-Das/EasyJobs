1322ac7aa3fd40cb13f3b78df983fd895f273b47
Sumeru <sumerudas2019@gmail.com>
2023-07-25 23:59:42 +0530

    deployed

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,5 +1,10 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import { RAPID_API_KEY } from '@env';
+
+
+// const rapidApiKey = RAPID_API_KEY
+
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -7,11 +12,11 @@ const useFetch = (endpoint, query) => {
   const [error, setError] = useState(null);
 
   const options = {
-    method: "GET",
+    method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
-      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+      'X-RapidAPI-Key': '37551549f7msh0537bd6cc66e6c5p1f8593jsnd39a4845251b',
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
     },
     params: { ...query },
   };
@@ -26,7 +31,7 @@ const useFetch = (endpoint, query) => {
       setIsLoading(false);
     } catch (error) {
       setError(error);
-      console.log(error)
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
